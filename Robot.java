ce746fd8385bfc799281dac45ba05570d867a2ea
Aaron Meese <ajmeese7@gmail.com>
2019-01-12 09:15:59 -0600

    Add files via upload
    Progress towards the new driving method for Parker

--- Robot.java
+++ Robot.java
@@ -32,6 +32,7 @@ public class Robot extends IterativeRobot {
 		CameraServer.getInstance().startAutomaticCapture();
 		
 		driveSelector.addDefault("Point", "Point");
+		driveSelector.addObject("Point w/ speed control", "Point w/ speed control");
 		driveSelector.addObject("Z-Axis", "Z-Axis");
 		SmartDashboard.putData("Driving styles", driveSelector);
 		
@@ -49,9 +50,47 @@ public class Robot extends IterativeRobot {
 			
 			leftSide.set(joystick.getX() + joystick.getY());
 			rightSide.set(joystick.getX() - joystick.getY());
-		} else if (driveStyle == "Z-Axis") {
-			// Slider determines speed
+		} else if (driveStyle == "Point w/ speed control") {
+			// Slider determines speed with point to drive
 			// PLAYERS: Parker
+			
+			double speed = (-joystick.getThrottle() + 1) / 2; // Negative because the controls are reversed
+			if (speed < .2) {
+				speed = .2;
+			}
+			
+			Boolean inXDeadzone = joystick.getX() > -.15 || joystick.getX() < .15;
+			double yValue = -joystick.getY();
+			if (inXDeadzone) {
+				if (yValue > .2) { // Forwards
+					leftSide.set(-speed);
+					rightSide.set(speed);
+				} else if (yValue < -.2) { // Backwards
+					leftSide.set(speed);
+					rightSide.set(-speed);
+				} else { // Stationary
+					leftSide.set(0);
+					rightSide.set(0);
+				}
+			} else {
+				double xValue = joystick.getX();
+				if (yValue > .2) { // Forwards
+					if (xValue > .15) {
+						leftSide.set(-speed);
+						rightSide.set(speed);
+					}
+					
+				} else if (yValue < -.2) { // Backwards
+					leftSide.set(speed);
+					rightSide.set(-speed);
+				} else { // Stationary
+					leftSide.set(0);
+					rightSide.set(0);
+				}
+			}
+		} else if (driveStyle == "Z-Axis") {
+			// Slider determines speed with z-axis turning
+			// PLAYERS: 
 			
 			double speed = (joystick.getThrottle() / 2) - .7;
 			if (joystick.getY() < -.3 || joystick.getZ() > .2 || joystick.getZ() < -.2) { // Forwards
@@ -82,3 +121,34 @@ public class Robot extends IterativeRobot {
 		//rightSide.set((joystick.getZ() - joystick.getY()) * .5);
 	}
 }
+
+
+/*
+int direction;
+if (joystick.getY() < -.2) {
+	// Forwards because reversed joystick
+	direction = 1;
+} else if (joystick.getY() > .2) {
+	// Backwards
+	direction = -1;
+} else {
+	// Stationary
+	direction = 0;
+}
+
+double speed = joystick.getThrottle();
+Boolean inXDeadzone = joystick.getX() > -.15 || joystick.getX() < .15;
+if (inXDeadzone) { // X-axis dead zone to make straight navigation easier
+	leftSide.set(-speed * direction);
+	rightSide.set(speed * direction);
+} else {
+	if (direction > 0) {
+		// Forwards
+		leftSide.set((joystick.getX() + speed + 1) * .65);
+		rightSide.set(-(joystick.getX() + speed + 1) * .65);
+	} else {
+		leftSide.set(-(joystick.getX() + speed + 1) * .65 * direction);
+		rightSide.set((joystick.getX() + speed + 1) * .65 * direction);
+	}
+}
+*/
